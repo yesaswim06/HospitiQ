b9e7c5ca698e2c9a3d48ca2a945fccda5a845da2
M N YESASWI BHARGAV <yesaswim06@gmail.com>
2026-09-21 13:15:15 +0530

    Update brand logo to transparent background with seamless dark and light theme matching

--- public/js/app.js
+++ public/js/app.js
@@ -106,6 +106,11 @@ function updateThemeIcons() {
     if (icon) icon.setAttribute('data-lucide', isDark ? 'sun' : 'moon');
   });
 
+  const logoSrc = isDark ? 'images/hospitiq_brand_logo_dark.png' : 'images/hospitiq_brand_logo_light.png';
+  document.querySelectorAll('.navbar-brand-img, .sidebar-brand-img, .login-brand-logo-img, .login-brand-logo img').forEach(img => {
+    img.src = logoSrc;
+  });
+
   lucide.createIcons();
 }
 
